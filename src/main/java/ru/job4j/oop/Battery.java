e1ccd9ab3d10d78c4ede473c4f09faa7e16c4cde
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-17 12:52:23 +0300

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -4,8 +4,15 @@ public class Battery {
     private int load;
 
     public void exchange(Battery another) {
-        int temp = this.load;
-        this.load -= another.load;
-        another.load += temp;
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery1 = new Battery();
+        Battery battery2 = new Battery();
+        battery1.load = 10;
+        battery2.load = 5;
+        battery1.exchange(battery2);
     }
 }
