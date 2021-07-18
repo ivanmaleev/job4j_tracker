2ea3a0b19fbb9b06d8b6471e35fdff225afd1255
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-18 10:40:22 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Ivanov Ivan Ivanych");
+        student.setGroup("12-03");
+        student.setDate(new Date());
+        System.out.println("ФИО " + student.getFio() + ", группа " + student.getGroup() + ", дата поступления " + student.getDate());
+    }
+}
