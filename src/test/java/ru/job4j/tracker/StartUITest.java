2f1898f5c5fc4252fbd055407abea16eec53caab
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-21 13:57:53 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #180062]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -36,8 +36,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "deleted item"
+                String.valueOf(item.getId())
         };
         StartUI.deteleItem(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
