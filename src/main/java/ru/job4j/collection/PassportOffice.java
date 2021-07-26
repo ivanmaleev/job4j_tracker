84e6a28f3d787e01c6cb78d620fa3dec9503391e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-26 19:13:55 +0300

    1. Паспорт и жители [#212656 #182032]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -9,7 +9,7 @@ public class PassportOffice {
 
     public boolean add(Citizen citizen) {
         boolean rsl = false;
-        if (get(citizen.getPassport()) == null) {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             rsl = true;
         }
