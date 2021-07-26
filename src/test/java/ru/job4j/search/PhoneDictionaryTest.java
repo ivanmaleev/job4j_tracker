d347d3f6834cf6ffff6fd462fcc77053ba3d9d0e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-07-26 12:24:26 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #181579]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -17,4 +17,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenFindByNameEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertThat(persons, is(new ArrayList<Person>()));
+    }
 }
