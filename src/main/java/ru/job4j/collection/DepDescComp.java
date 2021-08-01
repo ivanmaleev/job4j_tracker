5e4efe1dbae2052cbbf4203e19a465f917b438f4
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-01 12:41:42 +0300

    2. Отсортировать департаменты [#34352 #183579]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,15 +7,10 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] splitO1 = o1.split("/");
         String[] splitO2 = o2.split("/");
-        if (!(splitO1.length == 1 && splitO2.length == 1)) {
-            int minLength = Math.min(splitO1.length, splitO2.length);
-            for (int i = 1; i < minLength; i++) {
-                if (!splitO1[i].equals(splitO2[i])) {
-                    return splitO1[i].compareTo(splitO2[i]);
-                }
-            }
+        int rsl = splitO2[0].compareTo(splitO1[0]);
+        if (rsl == 0) {
             return o1.compareTo(o2);
         }
-        return o2.compareTo(o1);
+        return rsl;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Departments {
 
@@ -20,8 +17,11 @@ public class Departments {
     }
 
     public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
     }
 
     public static void sortDesc(List<String> orgs) {
+        DepDescComp depDescComp = new DepDescComp();
+        Collections.sort(orgs, depDescComp);
     }
 }
