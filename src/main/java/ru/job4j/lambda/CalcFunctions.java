7f52b641aba7ef2a63ba9ffa795e9f8715e94baa
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-02 10:06:24 +0300

    5. Подсчет функции в диапазоне. [#24251 #184392]

--- src/main/java/ru/job4j/lambda/CalcFunctions.java
+++ src/main/java/ru/job4j/lambda/CalcFunctions.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 public class CalcFunctions {
     public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         ArrayList<Double> doubleArrayList = new ArrayList<>();
-        for (int i = start; i <= end; i++) {
+        for (int i = start; i < end; i++) {
             doubleArrayList.add(func.apply((double) i));
         }
         return doubleArrayList;
