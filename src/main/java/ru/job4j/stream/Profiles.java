63811c5eea811068b8dc2106ccf6e0594633343e
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-03 12:42:29 +0300

    2. Список адресов [#110062 #184889]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
     }
 }
