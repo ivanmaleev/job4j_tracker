5a8dfd209ff7ec2310bfec02bb63ee4567ea92ca
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-05 19:39:59 +0300

    9. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,61 @@
+package ru.job4j.stream;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+    private int maxSpeed;
+    private int numberOfCylinders;
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private int horsePower;
+        private int maxSpeed;
+        private int numberOfCylinders;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildHorsePower(int horsePower) {
+            this.horsePower = horsePower;
+            return this;
+        }
+
+        Builder buildMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        Builder buildNumberOfCylinders(int numberOfCylinders) {
+            this.numberOfCylinders = numberOfCylinders;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.maxSpeed = maxSpeed;
+            car.horsePower = horsePower;
+            car.numberOfCylinders = numberOfCylinders;
+            return car;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder().buildBrand("Audi")
+                .buildModel("Q8")
+                .buildHorsePower(500)
+                .buildMaxSpeed(300)
+                .buildNumberOfCylinders(8)
+                .build();
+    }
+}
