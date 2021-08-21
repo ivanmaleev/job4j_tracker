73728d627d6f941033cfc68c69d98911f6dee98f
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-08-21 16:35:06 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -25,12 +26,23 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> returnNode = findByPredicate(node -> node.getValue().equals(value));
+        return returnNode;
+    }
+
+    @Override
+    public boolean isBinary() {
+        Optional<Node<E>> notBinaryNode = findByPredicate(node -> node.getChildren().size() > 2);
+        return !notBinaryNode.isPresent();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.getValue().equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
--- src/test/java/ru/job4j/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -40,4 +40,18 @@ public class SimpleTreeTest {
         assertFalse(tree.add(2, 6));
     }
 
+    @Test
+    public void whenBinaryOrNot() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        assertTrue(tree.isBinary());
+        tree.add(2, 4);
+        tree.add(2, 5);
+        assertTrue(tree.isBinary());
+        tree.add(2, 6);
+        assertFalse(tree.isBinary());
+    }
+
+
 }
